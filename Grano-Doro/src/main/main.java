7dff3cd28dcf5036afec4cd44dd37cfeaf50d60a
João Guilherme <Joo Guilherme@Joao>
2024-05-21 18:17:09 -0300

    .project removido

--- Grano-Doro/src/main/main.java
+++ Grano-Doro/src/main/main.java
@@ -3,7 +3,7 @@ package main;
 import Telas.Login; 
 
 public class main {
-
+//não definir como main do programa ainda
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Login login = new Login();

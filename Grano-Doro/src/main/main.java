fb2c548e962d3084ce50fb2a0b7767bb6a5dab96
João Guilherme <Joo Guilherme@Joao>
2024-05-21 18:14:43 -0300

    Novo package "Fundos", novos fundos, botão voltar na tela cadastro funcional, adiciona um botão para ver senha nas telas Login e Cadastro, cores dos textos mudadas de BLACK  para DARK GRAY, adição de um package e uma class "Main" (não funcional) mudanças na tela "Principal". Agora os .jar do banco de dados estão configurados para serem buscados diretamente da pasta bibliotecas do programa e não dentro do computado como anteriormente.

---
+++ Grano-Doro/src/main/main.java
@@ -0,0 +1,14 @@
+package main;
+
+import Telas.Login; 
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Login login = new Login();
+		login.setVisible(true);
+
+	}
+
+}
